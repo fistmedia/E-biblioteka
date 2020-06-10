f05c26ee044d4db43ec06a3000e672216269fe1f
marijana0 <marijana.stanisavljevic.95@gmail.com>
2020-06-11 00:26:17 +0200

    .......

--- app.py
+++ app.py
@@ -1,123 +1,146 @@
+#!/usr/bin/python
+from os import getenv
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, render_template, url_for, request, redirect, session, flash
-import mysql.connector
+from mysql.connector import connect
 from werkzeug.security import generate_password_hash, check_password_hash
+#from werkzeug.utils import secure_filename
 
-konekcija = mysql.connector.connect(
-	host="localhost",
-	user="root",
-	database="biblioteka"
+# Create the .env file's path and load it
+load_dotenv(join(dirname(__file__), '.env'))
+
+konekcija = connect(
+    host = getenv('DB_Host'),
+    user = getenv('DB_User'),
+    passwd = getenv('DB_Pass'),
+    database = getenv('DB_Db')
 )
-kursor = konekcija.cursor(dictionary=True)
 
-def ulogovan():
-	if 'ulogovani_korisnik' in session:
-		return True
-	else: return False
-def ulogovan_admin():
-	if 'ulogovani_admin' in session:
-		return True
-	else: return False
+kursor = konekcija.cursor(dictionary = True)
+
+def ulogovan(tip = 'korisnik'):
+    if 'ulogovani_korisnik' in session:
+        if tip == 'admin':
+            kursor.execute('SELECT bibliotekar FROM korisnik WHERE id=%s', (session['ulogovani_korisnik'],))
+            res = kursor.fetchone()
+            return res
+        else: return True
+    else: return False
 
 app = Flask(__name__)
-app.secret_key = '7wHtt8QURb'
-
-k_id=0
+app.secret_key = getenv('Cookie_secret')
 
 @app.route('/')
 def index():
-	sql = "SELECT * FROM vesti"
-	kursor.execute(sql)
-	vesti = kursor.fetchall()
-	if ulogovan():
-		return render_template('vesti_ulogovan.html', vesti=vesti)
-	else:
-		 return render_template('vesti.html', vesti=vesti)
+    sql = 'SELECT * FROM vesti'
+    kursor.execute(sql)
+    vesti = kursor.fetchall()
+    if ulogovan():
+        return render_template('vesti_ulogovan.html', vesti = vesti)
+    else:
+        return render_template('vesti.html', vesti = vesti)
 
 @app.route('/logout')
 def logout():
-	session.pop('ulogovani_korisnik', None)
-	return redirect(url_for('korisnici_login'))
+    session.pop('ulogovani_korisnik', None)
+    return redirect(url_for('korisnici_login'))
 
-@app.route('/logout_admin')
-def logout_admin():
-	session.pop('ulogovani_admin', None)
-	return redirect(url_for('korisnici_login'))
-
-@app.route('/korisnici_login', methods=['GET', 'POST'])
+@app.route('/korisnici_login', methods = ['GET', 'POST'])
 def korisnici_login():
-	if request.method == 'GET':
-		return render_template('korisnici_login.html')
-	elif request.method == 'POST':
-		forma = request.form
-		upit = "SELECT * FROM korisnik WHERE email=%s"
-		podaci = (forma["email"],)
-		kursor.execute(upit, podaci)
-		korisnik = kursor.fetchone()
-		if korisnik == None:
-			flash('E-mail je pogrešan')
-			return render_template('korisnici_login.html')
-		if check_password_hash(korisnik["lozinka"], forma["lozinka"]):
-			if korisnik["bibliotekar"]==0:
-				session['ulogovani_korisnik'] = str(korisnik)
-				return redirect(url_for('korisnik_ulogovan'))
-			if korisnik["bibliotekar"]==1:
-				session['ulogovani_admin']=str(korisnik)
-				return redirect(url_for('admin_ulogovan'))
-		else:
-			flash('Šifra je pogrešna')
-			return render_template('korisnici_login.html')
-	
+    if request.method == 'GET':
+        return render_template('korisnici_login.html')
+
+    forma = request.form
+    upit = 'SELECT * FROM korisnik WHERE email=%s'
+    podaci = (forma["email"],)
+    kursor.execute(upit, podaci)
+    korisnik = kursor.fetchone()
+    if korisnik == None:
+        flash('E-mail je pogrešan')
+        return render_template('korisnici_login.html')
+    if check_password_hash(korisnik['lozinka'], forma['lozinka']):
+        session['ulogovani_korisnik'] = korisnik['id']
+        if korisnik['bibliotekar'] == 0:
+            return redirect(url_for('korisnik_ulogovan'))
+        if korisnik['bibliotekar'] == 1:
+            return redirect(url_for('admin_ulogovan'))
+    else:
+        flash('Šifra je pogrešna')
+        return render_template('korisnici_login.html')
+
 @app.route('/korisnik_ulogovan')
 def korisnik_ulogovan():
-	if ulogovan():
-		return render_template('korisnik_ulogovan.html')
-	else:
-		return render_template('korisnici_login.html')	
+    if ulogovan():
+        return render_template('korisnik_ulogovan.html')
+    else:
+        return render_template('korisnici_login.html')    
 
 @app.route('/admin_ulogovan')
 def admin_ulogovan():
-	if ulogovan_admin():
-		sql = "SELECT * FROM vesti"
-		kursor.execute(sql)
-		vesti = kursor.fetchall()
-		return render_template('admin_ulogovan.html', vesti=vesti)
-	else:
-		return render_template('korisnici_login.html')
+    if ulogovan('admin'):
+        sql = 'SELECT * FROM vesti'
+        kursor.execute(sql)
+        vesti = kursor.fetchall()
+        return render_template('admin_ulogovan.html', vesti = vesti)
+    else:
+        return render_template('korisnici_login.html')
 
 @app.route('/admin_korisnici')
 def admin_korisnici():
-	if ulogovan_admin:
-		sql = "SELECT * FROM korisnici"
-		kursor.execute(sql)
-		korisnici = kursor.fetchall()
-		return render_template('admin_korisnici.html', korisnici=korisnici)
-	else:
-		 return render_template('korisnici_login.html')
+    if ulogovan('admin'):
+        sql = 'SELECT * FROM korisnici'
+        kursor.execute(sql)
+        korisnici = kursor.fetchall()
+        return render_template('admin_korisnici.html', korisnici = korisnici)
+    else:
+        return render_template('korisnici_login.html')
 
-@app.route('/registracija', methods=['GET', 'POST'])
+@app.route('/registracija', methods = ['GET', 'POST'])
 def registracija():
-	if request.method=="GET":
-		return render_template('registracija.html')
-	elif request.method == 'POST':
-		podaci = request.form
-		sql = """ INSERT INTO 
-				korisnik (ime, prezime, email, kontakt, lozinka) 
-				VALUES (%s,%s,%s,%s,%s)
-				"""
-		hes = generate_password_hash(podaci['lozinka'])
-		vrednosti = (podaci['ime'],podaci['prezime'],podaci['email'],podaci['kontakt'],hes)
-		kursor.execute(sql, vrednosti)
-		konekcija.commit()
-		return redirect(url_for('korisnici_login'))
+    if request.method == 'GET':
+        return render_template('registracija.html')
+
+    podaci = request.form
+    sql = '''INSERT INTO 
+            korisnik (ime, prezime, email, kontakt, lozinka) 
+            VALUES (%s,%s,%s,%s,%s)
+            '''
+    hes = generate_password_hash(podaci['lozinka'])
+    vrednosti = (podaci['ime'], podaci['prezime'], podaci['email'], podaci['kontakt'], hes)
+    kursor.execute(sql, vrednosti)
+    konekcija.commit()
+    return redirect(url_for('korisnici_login'))
 
 @app.route('/dostupne_knjige')
 def dostupne_knjige():
-	if ulogovan():
-		sql = "SELECT * FROM knjiga"
-		kursor.execute(sql)
-		knjige = kursor.fetchall()
-		return render_template('dostupne_knjige.html', knjige=knjige)
-	else:
-		return redirect(url_for('korisnici_login'))
+    if ulogovan():
+        sql = 'SELECT * FROM knjiga'
+        kursor.execute(sql)
+        knjige = kursor.fetchall()
+        return render_template('dostupne_knjige.html', knjige = knjige)
+    else:
+        return redirect(url_for('korisnici_login'))
 
-app.run(debug=True)
+@app.route('/uplata',  methods = ['GET', 'POST'])
+def uplata():
+    if ulogovan():
+        if request.method == 'GET':
+            return render_template('uplata.html')
+            
+        if request.files:
+            for name in request.files:
+                #join()
+                with open('static/uplatnice/' + name, 'w+') as file:
+                    upload = request.files[name]
+                    upload.save(file)
+                    kursor.execute('INSERT INTO uplatnica (korisnik_id, naziv_fajla) VALUES (%s, %s)', (session['ulogovani_korisnik'], name))
+
+            konekcija.commit()
+            flash('Uplatnica poslata')
+
+        return render_template('uplata.html')
+    else:
+        return redirect(url_for('korisnici_login'))
+
+app.run(debug = True)
