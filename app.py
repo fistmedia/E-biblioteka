459931c440385d9ff695b3de272412f19a1f498e
marijana0 <marijana.stanisavljevic.95@gmail.com>
2020-06-12 11:38:34 +0200

    .......

--- app.py
+++ app.py
@@ -88,8 +88,9 @@ def korisnik_ulogovan():
         print(kor_id)      
         kursor.execute(sql_knjige, kor_id)
         knjige=kursor.fetchall()
+        print(knjige)
         konekcija.commit()
-        return render_template('korisnik_ulogovan.html')
+        return render_template('korisnik_ulogovan.html', knjige=knjige)
     else:
         return render_template('korisnici_login.html')    
 
